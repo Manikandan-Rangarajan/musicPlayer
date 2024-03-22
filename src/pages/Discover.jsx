553a6202f9866598009d3be239ec27e1047dde1a
Manikandan <username270904@gmail.com>
2024-03-22 06:16:59 +0530

    music commit

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,16 +1,44 @@
 import { Error, Loader, SongCard} from '../components';
 import { genres } from '../assets/constants';
-import { useGetAlbumDetailsQuery } from '../redux/services/shazamCore'
+// import { useGetAlbumDetailsQuery } from '../redux/services/shazamCore'
+import { useEffect, useState } from 'react';
 
 const Discover = () => {
 
- const { data, isFetching, error} = useGetAlbumDetailsQuery();   
+//  const { data, isFetching, error} = useGetAlbumDetailsQuery();   
  const genreTitle = 'Pop';   
 
- console.log(data);
+ const [music, setMusic] = useState([]);
+
+ const getMusic = async() => {
+    const url = 'https://spotify23.p.rapidapi.com/search/?q=%3CREQUIRED%3E&type=multi&offset=0&limit=16&numberOfTopResults=10';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c343a4e2a7mshf63011433f1f3cdp1a5ce1jsne13e58cee6bd',
+		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+	setMusic(result);
+	console.log(result);
+} catch (error) {
+	setMusic(error);
+}
+ }
+
+ useEffect(()=>{
+    getMusic();
+ },[])
+
+ const msArray = [1,2,3,4,5,6,7,8,9,10]
+ const log = async()=>{ console.log(music?.albums?.data?.items)}
  
- if(isFetching) return  <Loader title='Loading Songs...'/>;
- if(error)      return  <Error />
+//  if(isFetching) return  <Loader title='Loading Songs...'/>;
+//  if(error)      return  <Error />
 
 
  return (
@@ -24,13 +52,13 @@ const Discover = () => {
             </select>
         </div>
         <div className='flex flex-wrap justify-center gap-8'>
-            {   data.map((song,i)=>(
-                <SongCard
-                 key={song.key}
-                 song = {song}
-                 i = {i}   
-                 data = {data}
+            {   music?.users?.items.map((musicData)=>(
+                <SongCard  
+                 imag = {musicData?.data?.image?.largeImageUrl} 
+                 song = {musicData?.data?.displayName}
+                
                  />
+                 
             ))}
         </div>
     </div>
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,16 +1,16 @@
 import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
-// const options = {
-//     method: 'GET',
-//     headers: {
-//         'X-RapidAPI-Key': 'c343a4e2a7mshf63011433f1f3cdp1a5ce1jsne13e58cee6bd',
-//         'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
-//     }
-// }
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'c343a4e2a7mshf63011433f1f3cdp1a5ce1jsne13e58cee6bd',
+        'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+    }
+}
 
-// fetch('https://spotify23.p.rapidapi.com/albums',options)
-// .then((response)=> response.json())
-// .then((response)=>console.log(response)).catch((err)=>{console.error(err)});
+fetch('https://spotify23.p.rapidapi.com/albums',options)
+.then((response)=> response.json())
+.then((response)=>console.log(response)).catch((err)=>{console.error(err)});
 
 export const shazamCore = createApi({
    reducerPath: 'shazamCore', // Redux Toolkit uses this to automatically generate the slice name for you
@@ -22,7 +22,7 @@ export const shazamCore = createApi({
    },
 }),
 endpoints: (builder) => ({
-    getAlbumDetails: builder.query({ query: ()=> '/albums?ids=3IBcauSj5M2A6lTeffJzdv'}),
+    getAlbumDetails: builder.query({ query: ()=> 'album_tracks?id=3IBcauSj5M2A6lTeffJzdv&offset=0&limit=30'}),
 }),
 
 });
@@ -30,3 +30,4 @@ endpoints: (builder) => ({
 export const{
    useGetAlbumDetailsQuery,
 } = shazamCore;
+
